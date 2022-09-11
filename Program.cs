e0a660d676cdd6b628365268f60b3f4e5fc73e54
TGabinW <slavabieliakov@gmail.com>
2022-09-11 19:10:11 +0300

    Di-project

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первый цвет для смешивания");
+string? fc = Console.ReadLine();
+Console.WriteLine("Введите второй цвет для смешивания");
+string? sc = Console.ReadLine();
+string Red = "Красный";
+string Blue = "Синий";
+string Yello = "Желтый";
+string Purpule = Red + Blue;
+string Orange = Red + Yello;
+string Green = Yello + Blue;
+if (fc + sc == Purpule)
+    Console.WriteLine("У вас получился фиолетовый");
+else if (fc + sc == Orange)
+    Console.WriteLine("У вас получился оранжевый");
+else if (fc + sc == Green)
+    Console.WriteLine("У вас получился зелёный");
+else
+    Console.WriteLine("Ошибка цвета");
